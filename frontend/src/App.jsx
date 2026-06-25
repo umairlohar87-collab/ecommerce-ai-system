31cfcdba749c28aef4ccdab6f73cedee8c2c3ee3
Saad Bin Riaz <140387256+SaadRiaz99@users.noreply.github.com>
2026-06-25 01:28:12 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,32 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Dashboard from "./pages/Dashboard";
+
+function App(){
+
+return(
+<BrowserRouter>
+
+<Routes>
+
+<Route path="/" element={<Home/>}/>
+
+<Route path="/products" element={<Products/>}/>
+
+<Route path="/cart" element={<Cart/>}/>
+
+<Route path="/dashboard" element={<Dashboard/>}/>
+
+</Routes>
+
+</BrowserRouter>
+)
+
+}
 
 function App() {
 return (
